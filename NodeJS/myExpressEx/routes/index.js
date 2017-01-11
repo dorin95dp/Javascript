89e881d0ed7f4b28bed4b745e038a808f049862a
Popa <dorin.popa1995@gmail.com>
2017-01-11 18:33:39 +0200

    Add my express example

---
+++ NodeJS/myExpressEx/routes/index.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Express',
+        name: "Dorin"
+    });
+});
+
+module.exports = router;
